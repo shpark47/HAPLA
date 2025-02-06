904e7b2c6c833f38af513ac8c270d5d36e0cd158
shpark47 <shp04078@naver.com>
2025-02-06 11:21:43 +0900

    index, main, 테스트용 컨트롤러

---
+++ src/main/java/com/hapla/test/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.hapla.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/main")
+    public String main() {
+        return "/main";
+    }
+}
