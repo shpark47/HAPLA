c4852b17037be0efa6dd1490038fd0c4cae5bbc7
Seonghun Park <shp04078@naver.com>
2025-02-20 20:25:45 +0900

    로그인

--- src/main/java/com/hapla/users/controller/UsersController.java
+++ src/main/java/com/hapla/users/controller/UsersController.java
@@ -3,27 +3,33 @@ package com.hapla.users.controller;
 import com.hapla.exception.Exception;
 import com.hapla.users.model.service.UsersService;
 import com.hapla.users.model.vo.Users;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
 @RequiredArgsConstructor
+@SessionAttributes("loginUser")
 public class UsersController {
 
     private final UsersService usersService;
 
     @PostMapping("/checkUser")
     @ResponseBody
-    public String checkUser(@RequestBody Users user) {
+    public String checkUser(@RequestBody Users user, HttpSession session) {
         int checkResult = usersService.checkUser(user);
         Users u = usersService.login(user);
         JSONObject json = new JSONObject();
         json.put("checkResult", checkResult);
         json.put("user", u);
+
+        session.setAttribute("loginUser", u);
 
         return json.toString();
     }
