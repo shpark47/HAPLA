6d629558e945e5299e0567911aca318e5ea600aa
shpark47 <shp04078@naver.com>
2025-03-05 15:38:27 +0900

    250305 메인정상화

--- src/main/java/com/hapla/common/controller/TestController.java
+++ src/main/java/com/hapla/common/controller/TestController.java
@@ -5,11 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-    @GetMapping("/main")
-    public String main() {
-        return "/main";
-    }
-
     @GetMapping("/recommend")
     public String recommend() {
         return "/recommend";
