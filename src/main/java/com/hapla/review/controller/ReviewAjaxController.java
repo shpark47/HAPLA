4b88ed8d3f9dd2a2b5ea83ab8a6a633809d81108
wajangchang <comy9905@naver.com>
2025-03-17 20:46:28 +0900

    250317 커뮤니티 및 리뷰 검색 성공

--- src/main/java/com/hapla/review/controller/ReviewAjaxController.java
+++ src/main/java/com/hapla/review/controller/ReviewAjaxController.java
@@ -1,40 +1,27 @@
 package com.hapla.review.controller;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.hapla.comm.model.service.CommService;
 import com.hapla.comm.model.vo.Reply;
 import com.hapla.review.model.service.ReviewService;
-import com.hapla.review.model.vo.Review;
-import com.hapla.users.model.vo.Users;
 
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import software.amazon.awssdk.awscore.exception.AwsServiceException;
-import software.amazon.awssdk.core.exception.SdkClientException;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.model.S3Exception;
 
 @RestController
 @RequestMapping({"/review", "/comm"})
@@ -101,6 +88,43 @@ public class ReviewAjaxController {
 	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
 	    }
 	}
+	
+	@PostMapping("/likes")
+	public ResponseEntity <Map<String, Object>> toggleLikes(@RequestParam("reviewNo") int reviewNo, @RequestParam("userNo") int userNo) {
+		Map<String, Object> response = new HashMap<>();
+
+	    try {
+	        System.out.println("좋아요 요청 - userNo: " + userNo + ", reviewNo: " + reviewNo);
+
+	        boolean isLiked = reviewService.checkUserLike(userNo, reviewNo) > 0;
+	        System.out.println("좋아요 상태 확인 - isLiked: " + isLiked);
+
+	        if (isLiked) {
+	            reviewService.removeLike(userNo, reviewNo);
+	        } else {
+	            reviewService.addLike(userNo, reviewNo);
+	        }
+
+	        int updatedLikeCount = reviewService.getLikeCount(reviewNo);
+	        
+	        System.out.println(updatedLikeCount);
+	        System.out.println("좋아요 개수 업데이트 완료 - updatedLikeCount: " + updatedLikeCount);
+
+	        
+	        response.put("status", "success");
+	        response.put("likes", updatedLikeCount);
+	        return ResponseEntity.ok(response);
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        response.put("status", "error");
+	        response.put("message", "서버 오류 발생: " + e.getMessage());
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+	    }
+	
+	
+	}
+	
 
 
 	
--- src/main/java/com/hapla/review/model/mapper/ReviewMapper.java
+++ src/main/java/com/hapla/review/model/mapper/ReviewMapper.java
@@ -3,6 +3,7 @@ package com.hapla.review.model.mapper;
 import java.util.ArrayList;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.session.RowBounds;
 
 import com.hapla.review.model.vo.Review;
@@ -18,5 +19,16 @@ public interface ReviewMapper {
 
 	int insertReview(Review r);
 
-	
+	int checkUserLike(@Param("userNo") int userNo, @Param("reviewNo") int reviewNo);
+
+	void insertLike(@Param("userNo") int userNo, @Param("reviewNo") int reviewNo);
+
+	void deleteLike(@Param("userNo") int userNo, @Param("reviewNo") int reviewNo);
+
+	int countLikes(int reviewNo);
+
+	int getSearchListCount(String search);
+
+	ArrayList<Review> searchReviewList(@Param("search") String search, RowBounds rowBounds);
+
 }
